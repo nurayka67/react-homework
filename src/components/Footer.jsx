976788fa20b71ba97abe973da5c245c4218af449
mobile2025 <kiraimomobam@gmail.com>
2025-09-01 14:45:16 +0300

    First commit: React homework project with components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>React Homework Application - Week 1</p>
+            <p>Created by <span className="highlight">Your Name</span></p>
+            <p>Submission deadline: 🗓️ September 2</p>
+        </footer>
+    );
+};
+
+export default Footer;
